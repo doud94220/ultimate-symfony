9f2e0b316a54080c2d8b97960b8512d069a7d572
Edouard ANTHONY <doud75@gmail.com>
2021-06-22 15:21:25 +0200

    Initiation au composant form

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Entity\Product;
+use App\Form\ProductType;
+use App\Repository\ProductRepository;
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/prout/{slug}", name="product_category")
+     */
+    public function category($slug, CategoryRepository $categoryRepository): Response
+    {
+        $category = $categoryRepository->findOneBy([
+            'slug' => $slug
+        ]);
+
+        //dd($category->getProducts());
+
+        if (!$category) {
+            //throw new NotFoundHttpException("La catégorie demandée n'existe pas");
+            throw $this->createNotFoundException("La catégorie demandée n'existe pas");
+        }
+
+        return $this->render('product/category.html.twig', [
+            'slug' => $slug,
+            'category' => $category
+        ]);
+    }
+
+    /**
+     * @Route("/{category_slug}/{slug}", name="product_show")
+     */
+    public function show($slug, ProductRepository $productRepository)
+    {
+        $product = $productRepository->findOneBy([
+            'slug' => $slug
+        ]);
+
+        if (!$product) {
+            throw $this->createNotFoundException("Le produit demandé n'existe pas...");
+        }
+
+        return $this->render('product/show.html.twig', [
+            'product' => $product
+        ]);
+    }
+    /**
+     * @Route("/admin/product/{id}/edit", name="product_edit")
+     */
+    public function edit($id, ProductRepository $productRepository, Request $request, EntityManagerInterface $em, UrlGeneratorInterface $urlGenerator)
+    {
+        $product = $productRepository->find($id);
+
+        $form = $this->createForm(ProductType::class, $product);
+        //$form->setData($product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            //$product = $form->getData(); //On peut l'enlever
+            $em->flush(); //pas besoin du persist => un peu bizarre
+
+            // $url = $urlGenerator->generate('product_show', [
+            //     'category_slug' => $product->getCategory()->getSlug(),
+            //     'slug' => $product->getSlug()
+            // ]);
+
+            //$response = new RedirectResponse($url);
+
+            return $this->redirectToRoute('product_show', [
+                'category_slug' => $product->getCategory()->getSlug(),
+                'slug' => $product->getSlug()
+            ]);
+        }
+
+        $formView = $form->createView();
+
+        return $this->render('product/edit.html.twig', [
+            'product' => $product,
+            'formView' => $formView
+        ]);
+    }
+
+    /**
+     * @Route("/admin/product/create", name="product_create")
+     */
+    public function create(Request $request, SluggerInterface $slugger, EntityManagerInterface $em)
+    {
+        $product = new Product;
+        $form = $this->createForm(ProductType::class, $product);
+
+        $form->handleRequest($request);
+
+        $data = $form->getData();
+
+        if ($form->isSubmitted()) {
+            //$product = $form->getData(); //Plus besoin de ça
+            $product->setSlug(strtolower($slugger->slug($product->getName())));
+
+            // $product = new Product;
+            // $product->setName($data['name'])
+            //     ->setShortDescription($data['shortDescription'])
+            //     ->setPrice($data['price'])
+            //     ->setCategory($data['category']);
+
+            $em->persist($product);
+            $em->flush();
+
+            return $this->redirectToRoute('product_show', [
+                'category_slug' => $product->getCategory()->getSlug(),
+                'slug' => $product->getSlug()
+            ]);
+        }
+
+        $formView = $form->createView();
+
+        return $this->render('product/create.html.twig', [
+            'formView' => $formView
+        ]);
+    }
+}
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,6 +6,7 @@ use Faker\Factory;
 use App\Entity\Product;
 use Liior\Faker\Prices;
 use App\Entity\Category;
+use Bezhanov\Faker\Provider\Commerce;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Symfony\Component\String\Slugger\SluggerInterface;
@@ -23,24 +24,24 @@ class AppFixtures extends Fixture
     {
         $faker = Factory::create();
         $faker->addProvider(new \Liior\Faker\Prices($faker));
-        //faker->addProvider(new \Bezh) ==> je peux pas le faire, j'ai pas pu installer le package
+        $faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($faker));
         $faker->addProvider(new \Bluemmb\Faker\PicsumPhotosProvider($faker));
 
         for ($c = 0; $c < 3; $c++) {
             $category = new Category;
-            $category->setName($faker->randomElement())
+            $category->setName($faker->department()) //faut mbezhanov
                 ->setSlug(strtolower($this->slugger->slug($category->getName())));
 
             $manager->persist($category);
 
             for ($p = 0; $p < mt_rand(15, 20); $p++) {
                 $product = new Product;
-                $product->setName($faker->sentence())
-                    ->setPrice($faker->price(4000, 20000))
+                $product->setName($faker->productName()) //faut mbezhanov
+                    ->setPrice($faker->price(4000, 20000)) //faut la class de Lior sur les faker prices
                     ->setSlug(strtolower($this->slugger->slug($product->getName())))
                     ->setCategory($category)
-                    ->setShortDescription($faker->paragraph())
-                    ->setMainPicture($faker->imageUrl(400, 400, true));
+                    ->setShortDescription($faker->paragraph()) //Faut php/faker je crois
+                    ->setMainPicture($faker->imageUrl(400, 400, true)); //Faut PicsumPhotosProvider
 
                 $manager->persist($product);
             }
